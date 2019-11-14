7f175348a648479a6e157fae83ab43806512dc90
omespef1 <omespef1@hotmail.com>
2019-11-14 16:27:04 -0500

    v
    v

--- src/assets/global.ts
+++ src/assets/global.ts
@@ -1,6 +1,6 @@
 export class Globals
         {
-           static readonly CentralizationUrl: string = 'http://consultoria.digitalware.co/seven_crm/centralizacion/api/GnConex/GetConnections';
+           static readonly CentralizationUrl: string = 'https://sac.digitalware.co:442/Seven_Crm/Centralizacion/api/GnConex/GetConnections';
            static ClientUrl :string ;
            static tracks: Array<{name: string,value:string,color:string}> =[{name:'Alta', value:'A',color:'hight'},{name:'Media',value:'M',color:'medium'},{name:'Baja',value:'',color:'low'} ]
         };
